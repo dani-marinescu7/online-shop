6544e42f2d406a5a2ffb8aff69ef37b9631cbe8e
Daniel <dani.marinescu7@gmail.com>
2024-02-12 20:33:16 +0200

    Initialized backend files

---
+++ src/test/java/org/dani/onlineshop/OnlineShopApplicationTests.java
@@ -0,0 +1,13 @@
+package org.dani.onlineshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineShopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
