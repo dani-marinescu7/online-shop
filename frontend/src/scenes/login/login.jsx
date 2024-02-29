2f5942383091807efe20c021f1fff828bf7792ac
Daniel <dani.marinescu7@gmail.com>
2024-02-29 22:54:56 +0200

    Fixed login form display problem

--- frontend/src/scenes/login/login.jsx
+++ frontend/src/scenes/login/login.jsx
@@ -53,96 +53,98 @@ const LoginPage = () => {
     };
 
     return (
-        <Box>
-            <Box
-                width="100%"
-                backgroundColor={theme.palette.background.alt}
-                p="1rem 6%"
-                textAlign="center"
-            >
-                <Typography fontWeight="bold" fontSize="32px" color="primary">
-                    The Sideline
-                </Typography>
-            </Box>
-
-            <Box
-                width={isNonMobileScreens ? "50%" : "93%"}
-                p="2rem"
-                m="2rem auto"
-                borderRadius="1.5rem"
-                backgroundColor={theme.palette.background.alt}
-            >
-                <Typography fontWeight="500" variant="h5" sx={{ mb: "1.5rem" }}>
-                    Text for login page
-                </Typography>
-                <Formik
-                    onSubmit={handleFormSubmit}
-                    initialValues={initialValues}
-                    validationSchema={schema}
+        <main className="content">
+            <Box>
+                <Box
+                    width="100%"
+                    backgroundColor={theme.palette.background.alt}
+                    p="1rem 6%"
+                    textAlign="center"
                 >
-                    {({
-                          values,
-                          errors,
-                          touched,
-                          handleBlur,
-                          handleChange,
-                          handleSubmit,
-                      }) => (
-                        <form onSubmit={handleSubmit}>
-                            <Box
-                                display="grid"
-                                gap="30px"
-                                gridTemplateColumns="repeat(4, minmax(0, 1fr))"
-                                sx={{
-                                    "& > div": { gridColumn: isNonMobile ? undefined : "span 4" },
-                                }}
-                            >
-                                <TextField
-                                    label="Email"
-                                    type="email"
-                                    onBlur={handleBlur}
-                                    onChange={handleChange}
-                                    value={values.newPassword}
-                                    name="email"
-                                    error={Boolean(touched.email) && Boolean(errors.email)}
-                                    helperText={touched.email && errors.email}
-                                    sx={{ gridColumn: "span 4" }}
-                                />
-                                <TextField
-                                    label="Password"
-                                    type="password"
-                                    onBlur={handleBlur}
-                                    onChange={handleChange}
-                                    value={values.confirmPassword}
-                                    name="password"
-                                    error={Boolean(touched.password) && Boolean(errors.password)}
-                                    helperText={touched.password && errors.password}
-                                    sx={{ gridColumn: "span 4" }}
-                                />
-                            </Box>
+                    <Typography fontWeight="bold" fontSize="32px" color="primary">
+                        The Sideline
+                    </Typography>
+                </Box>
 
-                            {/* BUTTONS */}
-                            <Box>
-                                <Button
-                                    fullWidth
-                                    type="submit"
+                <Box
+                    width={isNonMobileScreens ? "50%" : "93%"}
+                    p="2rem"
+                    m="2rem auto"
+                    borderRadius="1.5rem"
+                    backgroundColor={theme.palette.background.alt}
+                >
+                    <Typography fontWeight="500" variant="h5" sx={{ mb: "1.5rem" }}>
+                        Text for login page
+                    </Typography>
+                    <Formik
+                        onSubmit={handleFormSubmit}
+                        initialValues={initialValues}
+                        validationSchema={schema}
+                    >
+                        {({
+                              values,
+                              errors,
+                              touched,
+                              handleBlur,
+                              handleChange,
+                              handleSubmit,
+                          }) => (
+                            <form onSubmit={handleSubmit}>
+                                <Box
+                                    display="grid"
+                                    gap="30px"
+                                    gridTemplateColumns="repeat(4, minmax(0, 1fr))"
                                     sx={{
-                                        m: "2rem 0",
-                                        p: "1rem",
-                                        backgroundColor: palette.primary.main,
-                                        color: palette.background.alt,
-                                        "&:hover": { color: palette.primary.main },
+                                        "& > div": { gridColumn: isNonMobile ? undefined : "span 4" },
                                     }}
-                                    onClick={() => console.log("Submit")}
                                 >
-                                    SUBMIT
-                                </Button>
-                            </Box>
-                        </form>
-                    )}
-                </Formik>
+                                    <TextField
+                                        label="Email"
+                                        type="email"
+                                        onBlur={handleBlur}
+                                        onChange={handleChange}
+                                        value={values.newPassword}
+                                        name="email"
+                                        error={Boolean(touched.email) && Boolean(errors.email)}
+                                        helperText={touched.email && errors.email}
+                                        sx={{ gridColumn: "span 4" }}
+                                    />
+                                    <TextField
+                                        label="Password"
+                                        type="password"
+                                        onBlur={handleBlur}
+                                        onChange={handleChange}
+                                        value={values.confirmPassword}
+                                        name="password"
+                                        error={Boolean(touched.password) && Boolean(errors.password)}
+                                        helperText={touched.password && errors.password}
+                                        sx={{ gridColumn: "span 4" }}
+                                    />
+                                </Box>
+
+                                {/* BUTTONS */}
+                                <Box>
+                                    <Button
+                                        fullWidth
+                                        type="submit"
+                                        sx={{
+                                            m: "2rem 0",
+                                            p: "1rem",
+                                            backgroundColor: palette.primary.contrastText,
+                                            color: palette.background.alt,
+                                            "&:hover": { color: palette.primary.contrastText },
+                                        }}
+                                        onClick={() => console.log("Submit")}
+                                    >
+                                        SUBMIT
+                                    </Button>
+                                </Box>
+                            </form>
+                        )}
+                    </Formik>
+                </Box>
             </Box>
-        </Box>
+        </main>
     );
 };
 
