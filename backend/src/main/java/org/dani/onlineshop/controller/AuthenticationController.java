4dc4f3de40ae9dfbf376d5d345fa0a2748b2e1aa
Daniel <dani.marinescu7@gmail.com>
2024-03-05 21:45:47 +0200

    Working login and register

--- backend/src/main/java/org/dani/onlineshop/controller/AuthenticationController.java
+++ backend/src/main/java/org/dani/onlineshop/controller/AuthenticationController.java
@@ -10,8 +10,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @CrossOrigin(origins = "*")
+@RequestMapping("/auth")
 @Controller
 @RequiredArgsConstructor
 public class AuthenticationController {
